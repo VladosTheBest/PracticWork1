b18c2af3f3a628384bf5911cf7d79c4cc7d18fa4
VladTheBest <vlaryur4ik@gmail.com>
2018-03-13 12:04:31 +0200

    CreateTable

---
+++ CreateTable/CreateTable/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CreateTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_tableName = new System.Windows.Forms.TextBox();
+            this.tb_FirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.btCreate = new System.Windows.Forms.Button();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_tableName
+            // 
+            this.tb_tableName.Location = new System.Drawing.Point(12, 40);
+            this.tb_tableName.Name = "tb_tableName";
+            this.tb_tableName.Size = new System.Drawing.Size(302, 20);
+            this.tb_tableName.TabIndex = 0;
+            // 
+            // tb_FirstName
+            // 
+            this.tb_FirstName.Location = new System.Drawing.Point(12, 153);
+            this.tb_FirstName.Name = "tb_FirstName";
+            this.tb_FirstName.Size = new System.Drawing.Size(112, 20);
+            this.tb_FirstName.TabIndex = 1;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(202, 153);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(112, 20);
+            this.tbLastName.TabIndex = 2;
+            // 
+            // btCreate
+            // 
+            this.btCreate.Location = new System.Drawing.Point(386, 37);
+            this.btCreate.Name = "btCreate";
+            this.btCreate.Size = new System.Drawing.Size(75, 23);
+            this.btCreate.TabIndex = 3;
+            this.btCreate.Text = "Create";
+            this.btCreate.UseVisualStyleBackColor = true;
+            this.btCreate.Click += new System.EventHandler(this.btCreate_Click);
+            // 
+            // btInsert
+            // 
+            this.btInsert.Location = new System.Drawing.Point(386, 151);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(75, 23);
+            this.btInsert.TabIndex = 4;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.btInsert_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 246);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 173);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btSelect
+            // 
+            this.btSelect.Location = new System.Drawing.Point(386, 320);
+            this.btSelect.Name = "btSelect";
+            this.btSelect.Size = new System.Drawing.Size(75, 23);
+            this.btSelect.TabIndex = 6;
+            this.btSelect.Text = "Select";
+            this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "First Nme";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 443);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSelect);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.btCreate);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tb_FirstName);
+            this.Controls.Add(this.tb_tableName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_tableName;
+        private System.Windows.Forms.TextBox tb_FirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Button btCreate;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CreateTable/CreateTable/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateTable
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source = CR5-09\SQLEXPRESS;Initial Catalog=VLADOS_BASE; Integrated Security = true;");
+        string table = "";
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.ContextMenuStrip = getMenu();
+        }
+        private ContextMenuStrip getMenu()
+        {
+            ContextMenuStrip cms = new ContextMenuStrip();
+            cms.Items.Add("Delete");
+            cms.Items[0].Click += Delete;
+            return cms;
+
+        }
+
+        private void Delete(object sender, EventArgs e)
+        {
+            string query = $"delete from  {table} where id={(listBox1.SelectedItem as string)[0]};";
+            conn.Open();
+            SqlCommand command = new SqlCommand(query, conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            btSelect_Click(sender, e);
+        }
+
+        private void btCreate_Click(object sender, EventArgs e)
+        {
+            table = tb_tableName.Text;
+            string query = $"create table {tb_tableName.Text}(id int primary key identity," +
+                 "firstName varchar(25),secondName varchar(25));";
+            conn.Open();
+            SqlCommand command = new SqlCommand(query, conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        private void btInsert_Click(object sender, EventArgs e)
+        {
+            string query = $"insert into  {table} values('{tb_FirstName.Text}','{tbLastName.Text}')";
+            conn.Open();
+            SqlCommand command = new SqlCommand(query, conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            btSelect_Click(sender, e);
+        }
+
+        private void btSelect_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string query = $"select * from {table}";
+            conn.Open();
+            SqlCommand command = new SqlCommand(query, conn);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader[0] + " " + reader[1] + " " + reader[2] + "\n");
+            }
+            conn.Close();
+
+        }
+    }
+}
